14f2f4f0d8204183e588dc3eaf6eed191e89b127
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-14 14:57:32 +0600

    Appoinment banner

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import Banner from './Banner';
+import Contact from './Contact';
+
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonials';
+
+const Home = () => {
+    return (
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
